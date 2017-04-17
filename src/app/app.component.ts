559e8cd678a7f8e01a12bc90560a076590d244c6
keven <kevenx97ferreira@gmail.com>
2017-04-17 16:43:48 -0300

    add input-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'ÃÉÕ';
   date = this.getdate();
+  teste: string = '';
 
   constructor() {
 
