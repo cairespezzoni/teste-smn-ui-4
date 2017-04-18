b6ca33c4dfaa531650846d75d176c0fa948b95c5
treinaweb <hyankelwin@outlook.com>
2017-04-18 16:04:32 -0300

    add rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ÃÉÕ';
-  date = this.getdate();
+ 
 
   constructor() {
 
   }
-  getdate() {
-    let date = new Date();
-    date.setDate(date.getDate() + 1);
-    return date;
-  }
+ 
 }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  title = 'ÃÉÕ';
+  date = this.getdate();
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getdate() {
+    let date = new Date();
+    date.setDate(date.getDate() + 1);
+    return date;
+  }
+
+}
