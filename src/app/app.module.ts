18e066856b4e0c5239b3a8e4be63b553ed834e93
Antonio <antonio@engsolutions.com.br>
2017-04-18 14:04:43 -0300

    ui card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
@@ -10,6 +10,9 @@ import { SMNUI4Module } from 'smn-ui-v4';
 @NgModule({
   declarations: [
     AppComponent,
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   imports: [
     BrowserModule,
