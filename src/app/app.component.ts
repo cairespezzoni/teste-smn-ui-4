5ebebebbf63da0366251a2a25a60502a0a6baed4
keven <kevenx97ferreira@gmail.com>
2017-04-19 11:10:08 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
-  title = 'ÃÉÕ';
-  // date = this.getdate();
-  teste: string = '';
 
-  constructor() {
-
-  }
+  constructor() {}
  
 }
